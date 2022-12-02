653c75a37e5c47c40b942576ee0cda97c012d8a8
Rishikesh Sadanandan <rishikesh.sadanandan@gmail.com>
2022-12-01 19:15:26 -0600

    created flask app with handler() and get()

--- amplify/backend/function/invokeFunction/src/index.py
+++ amplify/backend/function/invokeFunction/src/index.py
@@ -1,15 +1,15 @@
-import json
+import awsgi
+from flask_cors import CORS
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+CORS(app)
+
+BASE_ROUTE = '/prompt'
+
+@app.route(BASE_ROUTE, methods=['GET'])
+def get_message():
+  return jsonify({'message': 'Hello from your new Amplify Python lambda!'})
 
 def handler(event, context):
-  print('received event:')
-  print(event)
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
-  }
+  return awsgi.response(app, event, context)
--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import { Amplify, API } from "aws-amplify";
 
 function App() {
+  const [text, setText] = useState("");
+
+  const myAPIName = "invokeAPI";
+  const path = "/prompt";
+
+  const handleTextAreaChange = (e) => {
+    console.log(e.target.value);
+    setText(e.target.value);
+    // send to backend amplify api function and turn to uppercase
+  };
+
+  // get the backend message amplify api function
+  const handleButtonClickMessage = async () => {
+    try {
+      const data = await API.get(myAPIName, path);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // send the text to the backend amplify api function
+  const handleButtonClickText = async () => {
+    const myInit = {
+      body: text,
+    };
+
+    try {
+      const response = await API.post(myAPIName, path, myInit);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +54,14 @@ function App() {
         >
           Learn React
         </a>
+        <br />
+        <textarea value={text} onChange={handleTextAreaChange} />
+        <br />
+        <button onClick={handleButtonClickMessage}>
+          Get message from backend!
+        </button>
+        <br />
+        <button onClick={handleButtonClickText}>Send text to backend!</button>
       </header>
     </div>
   );
